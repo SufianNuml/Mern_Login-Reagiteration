4b8b4d02f6f34f97e791363608e1005404468732
Sufian Aslam <sufianaslam127@gmail.com>
2023-10-17 01:34:11 +0500

    Update index.js
    changes done in backend index.js for cors

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,11 @@ const cors=require("cors");
 
 const app=express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["https://deploy-mern-lwhq.vercel.app"],
+    methods: ["POST", "GET],
+    credentials: true
+}));
 
 app.post("/login",async(req,resp)=>
 {
