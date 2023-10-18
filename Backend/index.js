8a3c6dc6fe6589fad82d2c422b25c1671bcc3e1a
Sufian Aslam <sufianaslam127@gmail.com>
2023-10-19 01:30:29 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const cors=require("cors");
 const app=express();
 app.use(express.json());
 app.use(cors({
-    origin: ["https://mern-login-reagiteration.vercel.app"],
+    origin: ["https://mern-login-reagiteration-frontend.vercel.app"],
     methods: ["POST", "GET"],
      credentials: true
 
